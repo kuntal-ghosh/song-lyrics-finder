eab9d6556b980fc2b2b6a86220829487223c8c10
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-08-17 21:46:17 +0600

    gh-pages branch updated

---
+++ index.js
@@ -0,0 +1,82 @@
+// getting reference
+const songName = document.getElementById("song-name");
+const searchBtn = document.getElementById("search");
+const searchResult = document.getElementById("search-result");
+const lyricsResult = document.getElementById("lyrics-result");
+const div = document.createElement("div");
+// div.innerHTML = "hello";
+
+searchBtn.addEventListener("click", () => {
+  // console.log(songName.value);
+  searchResult.innerHTML = "";
+  getSongBySongName(songName.value).then((songs) => {
+    console.log(songs);
+    let displayedSongNumber = 0;
+    if (songs.length < 10) {
+      displayedSongNumber = songs.length;
+    } else {
+      displayedSongNumber = 10;
+    }
+
+    for (let i = 0; i < displayedSongNumber; i++) {
+      console.log(songs[i]);
+      songObject = songs[i];
+      //   console.log(songObject);
+      const {
+        id,
+        title,
+        artist: { name: artistName },
+      } = songObject;
+      //   console.log(songs[i]);
+      //   div.innerHTML = songs[i].id;
+      searchResult.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
+        <div class="col-md-9">
+          <h3 class="lyrics-name">${title}</h3>
+          <p class="author lead">Album by <span>${artistName}</span></p>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+          <button class="btn btn-success" onclick="getLyrics('${title}', '${artistName}');">Get Lyrics</button>
+        </div>
+      </div>`;
+    }
+  });
+  songName.value = "";
+});
+
+function getSongBySongName(songName) {
+  return fetch(`https://api.lyrics.ovh/suggest/${songName}`)
+    .then((response) => response.json())
+    .then((songs) => songs.data);
+  //   return songs;
+}
+
+function getLyrics(title, artistName) {
+  if (title && artistName) {
+    fetch(`https://api.lyrics.ovh/v1/${artistName}/${title}`)
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          return response.json();
+        } else {
+          lyricsResult.innerHTML = `<div class="single-lyrics text-center">
+          <button class="btn go-back">&lsaquo;</button>
+          <h2 class="text-success mb-4">Sorry, don't have any lyrics for ${title} by ${artistName}</h2>
+        </div>`;
+          console.log("don't have any lyrics");
+          return 0;
+        }
+      })
+      .then((data) => {
+        if (data) {
+          console.log(data);
+          lyricsResult.innerHTML = `<div class="single-lyrics text-center">
+          <button class="btn go-back">&lsaquo;</button>
+          <h2 class="text-success mb-4">${title} By ${artistName}</h2>
+          <pre class="lyric text-white">
+         ${data.lyrics}
+          </pre>
+        </div>`;
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+}
