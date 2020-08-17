ce0426cb9c7c4ef73cfc4cae150b04fb0eccb269
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-08-17 22:29:45 +0600

    updated on gh-pages

--- index.js
+++ index.js
@@ -4,52 +4,67 @@ const searchBtn = document.getElementById("search");
 const searchResult = document.getElementById("search-result");
 const lyricsResult = document.getElementById("lyrics-result");
 const div = document.createElement("div");
-// div.innerHTML = "hello";
 
+// search button click events
 searchBtn.addEventListener("click", () => {
-  // console.log(songName.value);
   searchResult.innerHTML = "";
   getSongBySongName(songName.value).then((songs) => {
-    console.log(songs);
-    let displayedSongNumber = 0;
-    if (songs.length < 10) {
-      displayedSongNumber = songs.length;
-    } else {
-      displayedSongNumber = 10;
-    }
+    if (songs.length !== 0) {
+      let displayedSongNumber = 0;
+      if (songs.length < 10) {
+        displayedSongNumber = songs.length;
+      } else {
+        displayedSongNumber = 10;
+      }
 
-    for (let i = 0; i < displayedSongNumber; i++) {
-      console.log(songs[i]);
-      songObject = songs[i];
-      //   console.log(songObject);
-      const {
-        id,
-        title,
-        artist: { name: artistName },
-      } = songObject;
-      //   console.log(songs[i]);
-      //   div.innerHTML = songs[i].id;
-      searchResult.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
-        <div class="col-md-9">
-          <h3 class="lyrics-name">${title}</h3>
-          <p class="author lead">Album by <span>${artistName}</span></p>
-        </div>
-        <div class="col-md-3 text-md-right text-center">
-          <button class="btn btn-success" onclick="getLyrics('${title}', '${artistName}');">Get Lyrics</button>
-        </div>
-      </div>`;
+      for (let i = 0; i < displayedSongNumber; i++) {
+        songObject = songs[i];
+        const {
+          id,
+          title,
+          artist: { name: artistName },
+        } = songObject;
+
+        searchResult.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
+          <div class="col-md-9">
+            <h3 class="lyrics-name">${title}</h3>
+            <p class="author lead">Album by <span>${artistName}</span></p>
+          </div>
+          <div class="col-md-3 text-md-right text-center">
+            <button class="btn btn-success" onclick="getLyrics('${title}', '${artistName}');">Get Lyrics</button>
+          </div>
+        </div>`;
+      }
+    } else {
+      searchResult.innerHTML = `<div class="single-result row align-items-center my-3 p-3 text-center justify-content-center">
+      <div class="col-md-9">
+        <h3 class="lyrics-name ">Please provide valid song name</h3>
+      </div>
+    </div>`;
+    lyricsResult.innerHTML="";
     }
   });
   songName.value = "";
 });
 
+/**
+ *  this function fetch songs from api according to song name
+ *
+ * @param {*string} songName
+ * @returns list of song objects as a promise
+ */
 function getSongBySongName(songName) {
   return fetch(`https://api.lyrics.ovh/suggest/${songName}`)
     .then((response) => response.json())
     .then((songs) => songs.data);
-  //   return songs;
 }
 
+/**
+ *  this is a function that inputs lyrics in the lyrics-result section
+ *
+ * @param {*string} songName
+ * @returns lyrics of the song
+ */
 function getLyrics(title, artistName) {
   if (title && artistName) {
     fetch(`https://api.lyrics.ovh/v1/${artistName}/${title}`)
